a949368750ad89312711f3606ef61734650c44f5
Aleksey Fiodorov <saintfather1@yandex.ru>
2022-12-06 12:33:52 +0300

    [PP] - SQL optimization

--- pp_sql_testtask.sql
+++ pp_sql_testtask.sql
@@ -1,15 +1,7 @@
-SET @min_age = 7, @max_age = 17, @books_count = 2;
-select u.id, concat(u.first_name, ' ', u.last_name), ubc.author, ubc.book_names
-from (
-	select count(cub.book_id) count_books, cub.user_id, cb.author, GROUP_CONCAT(distinct cb.name SEPARATOR ', ') book_names
-    from (
-		select ub.user_id, ub.book_id
-		from user_books ub 
-		join users cu on cu.id = ub.user_id
-		where cu.age between @min_age and @max_age
-    ) cub
-    join books cb on cb.id = cub.book_id 
-    group by cub.user_id, cb.author
-    having count_books = @books_count
-) ubc
-join users u on u.id = ubc.user_id;
+SELECT users.id AS id, concat(users.first_name,' ‘,users.last_name) AS name, books.author AS author, GROUP_CONCAT(books.name SEPARATOR ‘,’) AS books
+FROM users
+LEFT JOIN user_books ON users.id = user_books.user_id
+LEFT JOIN books ON user_books.book_id = books.id
+WHERE age BETWEEN 7 AND 17
+GROUP BY users.id
+HAVING COUNT(user_books.book_id) = 2 AND COUNT(DISTINCT(books.author)) = 1;
